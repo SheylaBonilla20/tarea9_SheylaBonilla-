375d57bb2774cd1bccaf3b9d2b0d01299f65d61b
Sheyla <Sheyla@unitec.edu>
2019-09-14 02:46:31 -0600

    Tarea 9

--- public/javascript.js
+++ public/javascript.js
@@ -1,60 +1,47 @@
+// UTILS
 // Conectamos al servidor con socket.io
-const socket = io();
+var socket = io();
 
-socket.on('login', (message) => {
-    console.log(message);
-});
+// Evitar XSS usando escape()
+var escape = function(html) {
+    return $('<div>').text(html).html();
+};
 
-socket.on('message', (message) => {
-    console.log(message);
-});
 
-socket.on('logout', (message) => {
-    console.log(message);
-});
+
+// AUTH
 
 // Usar el modal de login con el usuario al entrar
-$('#login').model({ dismissible: false }).submit((e) => {
+$('#login').modal({ dismissible: false }).submit(function(e) {
     e.preventDefault();
-    let user = $('#login input').val();
+    var user = $('#login input').val();
 
     // Continuar sólo si sí que hay usuario
     if (!user) return;
     cookies({ user });
     $('#login').modal('close');
-
-    // Enviar el evento al resto de usuarios
     socket.emit('login', user);
-})
+    setTimeout(function() { $('#message').focus(); }, 500);
+});
 
-// Comunicar el usuario al resto de gente o pedir login
 if (cookies('user')) {
     socket.emit('login', cookies('user'));
+    setTimeout(function() { $('#message').focus(); }, 500);
 } else {
     $('#login').modal('open');
 }
 
-// El usuario envía un mensaje
-$('form.message').submit((e) => {
-    e.preventDefault();
-    let $input = $(e.target).find('input');
-    let text = $input.val();
-
-    // Borrar el mensaje del input
-    $input.val('');
-
-    // Enviar el mensaje a todos
-    socket.emit('message', text);
+$('.logout').click(function() {
+    cookies({ user: null });
+    window.location.reload();
 });
 
-// Evitar XSS usando escape()
-let escape = (html) => {
-    return $('<div>').text(html).html();
-};
 
+
+// MENSAJES
 
 // Añadir un mensaje a la lista y hacer scroll
-let add = (html) => {
+var add = function(html) {
     var toScroll = $('.messages').prop("scrollHeight") - 50 < $('.messages').scrollTop() + $('.messages').height();
     $('.messages').append(html);
 
@@ -66,21 +53,35 @@ let add = (html) => {
     }
 };
 
-socket.on('login', (message) => {
+// El usuario envía un mensaje
+$('form.message').submit(function(e) {
+    e.preventDefault();
+    var $input = $(e.target).find('input');
+    var text = $input.val();
+
+    // Borrar el mensaje del input
+    $input.val('');
+
+    // Enviar el mensaje a todos
+    socket.emit('message', text);
+});
+
+
+socket.on('login', function(message) {
     add('<div class="msg login">\
-      <span class="user">' + escape(message.user) + '</span> logged in.\
-    </div>');
+    <span class="user">' + escape(message.user) + '</span> logged in.\
+  </div>');
 });
 
-socket.on('message', (message) => {
+socket.on('message', function(message) {
     add('<div class="msg">\
-      <span class="user">' + escape(message.user) + ':</span> \
-      <span class="msg">' + escape(message.text) + '</span>\
-    </div>');
+    <span class="user">' + escape(message.user) + ':</span> \
+    <span class="msg">' + escape(message.text) + '</span>\
+  </div>');
 });
 
-socket.on('logout', (message) => {
+socket.on('logout', function(message) {
     add('<div class="msg logout">\
-      <span class="user">' + escape(message.user) + '</span> logged out.\
-    </div>');
+    <span class="user">' + escape(message.user) + '</span> logged out.\
+  </div>');
 });
